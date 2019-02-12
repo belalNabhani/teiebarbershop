caa03c5c41d9d170229d181f33f0abacdaa35cdd
Belal Nabhani <bel.nabhani@gmail.com>
2019-02-12 18:10:45 +0200

    fix spelling

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -35,7 +35,7 @@ const Prices = () => (
               <div className="voffset40" />
               <h4 className="titlefeat">Skjegg Trim/Forming</h4>
               <div className="voffset30" />
-              <p className="textfeat">Få et komplett skjegg trim og gi det en ren, naturlig stil.<br/>250 kr</p>
+              <p className="textfeat">Få et komplett skjegg trim og gi det en ren, naturlig stil.</p>
               <p>Hot towel shave 350 kr</p>
             </div>
           </div>
@@ -46,7 +46,7 @@ const Prices = () => (
               <div className="voffset40" />
               <h4 className="titlefeat">Shave</h4>
               <div className="voffset30" />
-              <p className="textfeat">For best resultat kombinerer vi moderne og tradisjonelle metoder.<br/>250 kr</p>
+              <p className="textfeat">For best resultat kombinerer vi moderne og tradisjonelle metoder.</p>
               <p>Hot towel shave 350 kr</p>
             </div>
           </div>
@@ -68,10 +68,10 @@ const Prices = () => (
               <div className="voffset40" />
               <img src={cream} />
               <div className="voffset40" />
-              <h4 className="titlefeat">Behandlig</h4>
+              <h4 className="titlefeat">Behandling</h4>
               <div className="voffset30" />
-              <p className="textfeat">Vi kan fjerne hår som plager deg i ansiktet og øre med vax eller tråd.</p>
-              <p>Pris fra 75 kr</p>
+              <p className="textfeat">Vi kan fjerne hår som plager deg i ansiktet og øre med voks eller tråd.</p>
+              <p>Pris fra 100 kr</p>
             </div>
           </div>
           <div className="col-sm-4">
@@ -81,7 +81,7 @@ const Prices = () => (
               <div className="voffset40" />
               <h4 className="titlefeat">Barnklipp</h4>
               <div className="voffset30" />
-              <p className="textfeat">Elder fra 5 år til 10 år<br/>250 kr</p>
+              <p className="textfeat">Alder fra 5 år til 10 år<br/>250 kr</p>
               <p>Fra 11 til 15 år</p>
               <p>300 kr</p>
             </div>
