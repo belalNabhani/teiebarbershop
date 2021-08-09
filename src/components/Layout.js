465b27ed2a93dc5e1c027f5c5f0c312daa73edf4
belal <bel.nabhani@gmail.com>
2021-08-09 23:19:06 +0300

    fixed path

---
+++ src/components/Layout.js
@@ -0,0 +1,31 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import * as React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { siteMetadata } from "../../gatsby-config"
+
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Helmet>
+        <meta name="author" description={siteMetadata.author} />
+        <title>{siteMetadata.title}</title>
+        <link rel="icon" href="assets/favicon.png" type="image/x-icon"/>
+      </Helmet>
+      {children}
+  </div>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
