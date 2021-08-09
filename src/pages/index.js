3206f3d572e2c7b9bff9a85d4f25786a26d21680
belal <bel.nabhani@gmail.com>
2021-08-09 23:16:37 +0300

    test2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import * as React from "react"
 import "../assets/css/main.css";
 import "../assets/css/vendor.css";
 
+import Layout from "../components/Layout"
+import Seo from "../components/seo"
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import About from "../components/About";
@@ -11,8 +13,7 @@ import OpeningHours from "../components/OpeningHours";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+
 
 const IndexPage = () => (
   <Layout>
